38bcd784da638a6c45c330872fc90b852c802976
navin.kumar <sndpkmr80ds@gmail.com>
2018-07-15 04:53:46 +0530

    dynamic menu active class apply

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,35 @@
 $(function() {
-	
+
 	// Cache the Window object
 	var $window = $(window);
-	
+
 	// Parallax Backgrounds
 	// Tutorial: http://code.tutsplus.com/tutorials/a-simple-parallax-scrolling-technique--net-27641
-	
+
 	$('section[data-type="background"]').each(function(){
 		var $bgobj = $(this); // assigning the object
-		
+
 		$(window).scroll(function() {
-		
+
 			// Scroll the background at var speed
-			// the yPos is a negative value because we're scrolling it UP!								
+			// the yPos is a negative value because we're scrolling it UP!
 			var yPos = -($window.scrollTop() / $bgobj.data('speed'));
-			
+
 			// Put together our final background position
 			var coords = '50% '+ yPos + 'px';
-			
+
 			// Move the background
 			$bgobj.css({ backgroundPosition: coords });
-			
+
 		}); // end window scroll
 	});
-	
+
+});
+
+$(document).ready(function () {
+		var url = window.location;
+		$('ul.nav a[href="'+ url +'"]').parent().addClass('active');
+		$('ul.nav a').filter(function() {
+				 return this.href == url;
+		}).parent().addClass('active');
 });
